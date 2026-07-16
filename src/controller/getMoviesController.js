8067e4dd50b38a23a5aa00b3697e17b1cb0f535f
olumild <olumild998@gmail.com>
2026-07-16 21:29:03 +0100

    get controller

--- src/controller/getMoviesController.js
+++ src/controller/getMoviesController.js
@@ -3,9 +3,8 @@ import { prisma } from "../config/db.js"
 
 const getMoviesController = async (req, res) => {
 
-    const getAllMovies = await prisma.movie.findMany();
 
-    res.json({ movies: getAllMovies })
+    res.send("trust and obey");
 
 }
 
